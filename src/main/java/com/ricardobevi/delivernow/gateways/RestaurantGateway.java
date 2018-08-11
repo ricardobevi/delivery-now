f3c7282b1cd24480b7735d15a392afe51feb3f43
Ricardo Bevilacqua <bevilacqua.ricardo@gmail.com>
2018-08-11 12:44:30 -0300

    Adds first (mocked) implementation of RestaurantGateway

---
+++ src/main/java/com/ricardobevi/delivernow/gateways/RestaurantGateway.java
@@ -0,0 +1,16 @@
+package com.ricardobevi.delivernow.gateways;
+
+import com.ricardobevi.delivernow.dto.RestaurantDto;
+
+public class RestaurantGateway {
+
+	public RestaurantDto getRestaurantFromId(String restaurantId){
+		return new RestaurantDto();
+	}
+
+	public void save(RestaurantDto asDto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
