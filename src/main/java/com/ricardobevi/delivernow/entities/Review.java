99fb365fd268dc2cac14851a89d919ab9382051b
Ricardo Bevilacqua <bevilacqua.ricardo@gmail.com>
2018-08-11 12:45:57 -0300

    Adds asDto method to Review entity

--- src/main/java/com/ricardobevi/delivernow/entities/Review.java
+++ src/main/java/com/ricardobevi/delivernow/entities/Review.java
@@ -1,5 +1,7 @@
 package com.ricardobevi.delivernow.entities;
 
+import com.ricardobevi.delivernow.dto.ReviewDto;
+
 public class Review {
 
 	private final String name;
@@ -13,8 +15,22 @@ public class Review {
 		this.rating = rating;
 	}
 	
+	public Review(ReviewDto reviewDto) {
+		this(reviewDto.name, reviewDto.review, new Rating(reviewDto.rating));
+	}
+
 	public Rating getRating() {
 		return this.rating;
+	}
+	
+	public ReviewDto asDto() {
+		ReviewDto reviewDto = new ReviewDto();
+		
+		reviewDto.name = this.name;
+		reviewDto.review = this.review;
+		reviewDto.rating = this.rating.asDouble();
+		
+		return reviewDto;
 	}
 	
 }
