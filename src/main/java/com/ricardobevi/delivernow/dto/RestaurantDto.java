8ce872bca4b20f45ae2eb614545487ebab6ddb43
Ricardo Bevilacqua <bevilacqua.ricardo@gmail.com>
2018-08-11 19:27:51 -0300

    Adds field id to RestaurantDto

--- src/main/java/com/ricardobevi/delivernow/dto/RestaurantDto.java
+++ src/main/java/com/ricardobevi/delivernow/dto/RestaurantDto.java
@@ -4,11 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RestaurantDto {
+	
+	public Long id;
+	
 	public Double rating;
 	public List<ReviewDto> reviews;
 	
 	public RestaurantDto() {
+		this.id = 0L;
 		this.reviews = new ArrayList<ReviewDto>();
+	}
+
+	public RestaurantDto(Long id) {
+		this.id = id;
 	}
 	
 }
