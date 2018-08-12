3683418a8929bae496b88429f478d8d4137426b2
Ricardo Bevilacqua <bevilacqua.ricardo@gmail.com>
2018-08-11 21:43:32 -0300

    Refactors ReviewDto to use constructor instead of public fields

--- src/main/java/com/ricardobevi/delivernow/dto/ReviewDto.java
+++ src/main/java/com/ricardobevi/delivernow/dto/ReviewDto.java
@@ -2,18 +2,26 @@ package com.ricardobevi.delivernow.dto;
 
 public class ReviewDto {
 
-	public String name;
-	public String review;
-	public Double rating;
-	
-	public ReviewDto() {}
+	private final String name;
+	private final String review;
+	private final Double rating;
 	
 	public ReviewDto(String name, String review, Double rating) {
 		this.name = name;
 		this.review = review;
 		this.rating = rating;
 	}
-	
-	
+
+	public String getName() {
+		return name;
+	}
+
+	public Double getRating() {
+		return rating;
+	}
+
+	public String getReview() {
+		return review;
+	}
 	
 }
